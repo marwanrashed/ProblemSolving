0d9ef67345ce18f0e642b22d381d4158fef9b142
marwanrashed <maro.isme2010@gmail.com>
2020-12-26 03:42:16 -0500

    Insertion on DOubly Linked List

---
+++ LinkedList/InsertDoublyLinkedList.cpp
@@ -0,0 +1,165 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class DoublyLinkedListNode {
+    public:
+        int data;
+        DoublyLinkedListNode *next;
+        DoublyLinkedListNode *prev;
+
+        DoublyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
+
+class DoublyLinkedList {
+    public:
+        DoublyLinkedListNode *head;
+        DoublyLinkedListNode *tail;
+
+        DoublyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            DoublyLinkedListNode* node = new DoublyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+                node->prev = this->tail;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_doubly_linked_list(DoublyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_doubly_linked_list(DoublyLinkedListNode* node) {
+    while (node) {
+        DoublyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the sortedInsert function below.
+
+/*
+ * For your reference:
+ *
+ * DoublyLinkedListNode {
+ *     int data;
+ *     DoublyLinkedListNode* next;
+ *     DoublyLinkedListNode* prev;
+ * };
+ *
+ */
+DoublyLinkedListNode* sortedInsert(DoublyLinkedListNode* head, int data) {
+    if (head == nullptr){
+        head = new DoublyLinkedListNode (data);
+        return head;
+    }
+    
+
+    DoublyLinkedListNode* current = head;
+    DoublyLinkedListNode* temp =  new DoublyLinkedListNode(data);
+
+    
+    if (head->next ==  nullptr){
+        if (data >= head->data){
+        temp->prev = head;
+        head->next = temp;
+        temp->next = head->next;
+        return head; }
+        else {
+        head->prev = temp;
+        temp->next = head;
+        return head;
+        }
+    }
+        if (head->prev == nullptr){
+            if (data <= head->data){ 
+                head->prev = temp;
+                temp->next = head; 
+            return temp;           }
+        }
+        
+    while (current){
+        if (current-> next != nullptr){
+            if ((data >= current->data) && (data <= current->next->data)){
+                temp->prev = current;
+                temp->next = current->next; 
+                current->next = temp;
+                return head;
+                }
+        }
+        else {
+            if (data >= current->data){
+            temp->prev = current; 
+            current->next = temp;
+            return head;
+            }
+        }
+        current = current->next;
+    } 
+        return head;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        DoublyLinkedList* llist = new DoublyLinkedList();
+
+        int llist_count;
+        cin >> llist_count;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        for (int i = 0; i < llist_count; i++) {
+            int llist_item;
+            cin >> llist_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            llist->insert_node(llist_item);
+        }
+
+        int data;
+        cin >> data;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        DoublyLinkedListNode* llist1 = sortedInsert(llist->head, data);
+
+        print_doubly_linked_list(llist1, " ", fout);
+        fout << "\n";
+
+        free_doubly_linked_list(llist1);
+    }
+
+    fout.close();
+
+    return 0;
+}
